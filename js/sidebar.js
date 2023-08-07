cb465c77383db010a432a11f2c7bf04b1f72b2fc
jhs <jsp7406@naver.com>
2023-08-07 17:50:37 +0900

    feat: "todolist test 0807"

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,13 +1,13 @@
 const sidebarToggleButtonOnClickHandle = () => {
     const sidebar = document.querySelector(".sidebar");
-    const sidebarToggleButton = document.querySelector(".sidebar-toggle-button");
+    // const sidebarToggleButton = document.querySelector(".sidebar-toggle-button");
 
     if(sidebar.classList.contains("isSidebarOpen")) {
         sidebar.classList.remove("isSidebarOpen");
-        sidebarToggleButton.innerHTML = '▶';
+        // sidebarToggleButton.innerHTML = '▶';
     }else {
         sidebar.classList.add("isSidebarOpen");
-        sidebarToggleButton.innerHTML = '◀';
+        // sidebarToggleButton.innerHTML = '◀';
     }
 }
 
